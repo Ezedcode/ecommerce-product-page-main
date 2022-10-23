f961ceaf4f2ab449e47ea2b6af0aff181a430bdd
Ezedélio Garcia <96814597+Ezedcode@users.noreply.github.com>
2022-10-23 12:52:11 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Global from "./theme/Global";
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Global />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/theme/Global.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+  }
+`;
+
+export default Global;
